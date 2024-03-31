073db30d37788d243855bac4c32fa9a5cc2705d4
ramiro-navas <ramiro.navas.dev@gmail.com>
2024-03-31 07:50:52 -0500

    feat: :sparkles: change modal icon
    changed mondal icon to close window

--- src/components/ui/Form.tsx
+++ src/components/ui/Form.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Input } from "./index";
+import { RxCross1 } from 'react-icons/rx'
 
 type Props = {
   setFormState: (formState: boolean) => void,
@@ -10,11 +11,11 @@ export function Form(props: Props) {
     <div className="w-screen h-screen fixed top-0 left-0 flex items-center justify-center bg-semi">
       <form className="w-600 h-200 rounded-16 bg-back grid px-86 items-center relative">
         <button 
-        className="text-20 absolute top-2 right-4"
+        className="text-20 text-titles absolute top-2 right-4"
         onClick={(e: any)=>{
           e.preventDefault();
           props.setFormState(false);
-        }}>x</button>
+        }}> <RxCross1 /> </button>
         <h2 className="font-roboto font-extrabold text-titles text-30 text-center">Nuevo objetivo</h2>
         <div className="flex">
         <Input placeholder="Objetivo" type="text" name="title" required />
