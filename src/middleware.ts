4290b6f2e62d357eeb47f473ede64a046e407cb0
Iván Navas <ivanrng.work@gmail.com>
2024-05-15 23:20:23 -0500

    fix: :bug: fixed middleware
    fixed jwtVerify and protect routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
+
 export const middleware = async (request: any) => {
   const token = request.cookies.get("token");
 
@@ -8,12 +9,23 @@ export const middleware = async (request: any) => {
       return NextResponse.redirect(new URL("/page/login", request.url));
     }
     try {
-      const ob = await jwtVerify(token.value, process.env.SECRET_VALUE);
-      console.log(ob)
+      const ob = await jwtVerify(
+        token.value,
+        new TextEncoder().encode(process.env.SECRET_VALUE)
+      );
+      console.log(ob);
     } catch (error) {
-      console.log(error)
-      //return NextResponse.redirect(new URL("/page/login", request.url));
+      console.log(error);
+      return NextResponse.redirect(new URL("/page/login", request.url));
+    }
+    return NextResponse.next();
+  }
+  if (
+    request.nextUrl.pathname.includes("/page/login") ||
+    request.nextUrl.pathname.includes("/page/register")
+  ) {
+    if (token !== undefined) {
+      return NextResponse.redirect(new URL("/page/feed", request.url));
     }
   }
-  return NextResponse.next();
 };
