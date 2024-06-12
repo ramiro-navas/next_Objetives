3406542c5d7888e183fdc2c32d9f3b7affd937ed
Iván Navas <ivanrng.work@gmail.com>
2024-06-11 21:08:02 -0500

    feat: :sparkles: add caroucel animation
    cange caroucel componet after 5 seconds

--- src/components/home/Caroucel.tsx
+++ src/components/home/Caroucel.tsx
@@ -2,12 +2,21 @@ import React, { useState, useEffect } from "react";
 import CaroucelCard from "../ui/CaroucelCard";
 import { useAppContext } from "@/Context";
 function Caroucel() {
-  const {caroucel, caroucelState, setCaroucelOb, page, caroucelOb } = useAppContext();
+  const { caroucel, caroucelState, setCaroucelOb, page, setPage, caroucelOb } = useAppContext();
   
   useEffect(()=>{
     setCaroucelOb(caroucelState.caroucel[page]);
+    setTimeout(() => {
+      if(page < caroucelState.caroucel.length - 1 ){
+        setPage(page + 1);
+        console.log(page);
+      }
+      if(page == caroucelState.caroucel.length -1){
+        setPage(0);
+        console.log(page);
+      } 
+    }, 5000);
   })
-  
 
   return (
     <>
